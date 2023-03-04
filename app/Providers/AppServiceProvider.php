4c288fa93b473a689b84ac87f601ac50a26e4e62
OTAVIO <OTAVIO.NICOLLAU@GMAIL.COM>
2023-03-04 16:09:58 -0300

    04/03/2023 - ajustes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,7 +30,7 @@ class AppServiceProvider extends ServiceProvider
         FacadesRateLimiter::for('prova', fn()=> Limit::perMinute(6));
         FacadesRateLimiter::for('cargos', fn()=> Limit::perMinute(6));
         FacadesRateLimiter::for('orgaos', fn()=> Limit::perMinute(6));
-        FacadesRateLimiter::for('questoes', fn()=> Limit::perMinute(6));
+        FacadesRateLimiter::for('questoes', fn()=> Limit::perMinute(30));
         FacadesRateLimiter::for('editais', fn()=> Limit::perMinute(6));
         FacadesRateLimiter::for('comentario', fn()=> Limit::perMinute(6));
         FacadesRateLimiter::for('downloads', fn()=> Limit::perMinute(6));
